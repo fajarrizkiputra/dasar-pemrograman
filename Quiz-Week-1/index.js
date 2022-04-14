d7b1427a700a390b65aff3fda4fdb6f812ff6da1
Fajar Rizki Putra <97790513+fajarrizkiputra@users.noreply.github.com>
2022-04-15 03:54:12 +0700

    Add files via upload

---
+++ Quiz-Week-1/index.js
@@ -0,0 +1,33 @@
+// JAWABAN SOAL 1
+var kalimat_1 = " Halo nama saya Muhammad Iqbal Mubarok "
+var kalimat_2 = " Saya Iqbal"
+var kalimat_3 = " Saya Muhammad Iqbal Mubarok "
+
+console.log(kalimat_1.length);
+console.log(kalimat_2.length);
+console.log(kalimat_3.length);
+
+// JAWABAN SOAL 2
+var tanggal = 29
+var bulan = 2
+var tahun = 2020
+
+next_date(tanggal , bulan , tahun ) // output : 1 Maret 2020
+console.log(next_date)
+
+// contoh 2
+
+var tanggal = 28
+var bulan = 2
+var tahun = 2021
+
+next_date(tanggal , bulan , tahun ) // output : 1 Maret 2021
+console.log(next_date)
+// contoh 3
+
+var tanggal = 31
+var bulan = 12
+var tahun = 2020
+
+next_date(tanggal , bulan , tahun ) // output : 1 Januari 2021
+console.log(next_date)
